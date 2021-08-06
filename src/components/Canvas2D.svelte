dfdc8f703db25a85928b9d658c4510c04da70bed
Paul Delhanty <paul.delhanty_git@cadcambridge.net>
2021-08-07 08:58:28 +0900

    refactor: move the bulk of Typescript code out of `.svelte` files.

--- src/components/Canvas2D.svelte
+++ src/components/Canvas2D.svelte
@@ -14,37 +14,10 @@
     }
   });
 
-  function hinomaru(context: CanvasRenderingContext2D, x: number, y: number, radius: number) {
-    context.fillStyle = '#ff0000';
-    context.beginPath();
-    context.arc(x, y, radius, 0, Math.PI * 2.0);
-    context.fill();
-  }
-  function grid(context: CanvasRenderingContext2D, spacing: number) {
-    const x0 = 0.5;
-    const x1 = canvas.width;
-    const y0 = top + 0.5;
-    const y1 = canvas.height;
-
-    context.beginPath();
-
-    for (var x = x0; x < x1; x += spacing) {
-      context.moveTo(x, y0);
-      context.lineTo(x, y1);
-    }
-
-    for (var y = y0; y < y1; y += spacing) {
-      context.moveTo(x0, y);
-      context.lineTo(x1, y);
-    }
-
-    context.strokeStyle = 'blue'; //"rgb(0.8,0.8,0.8)"; //#888; //"black";
-    context.stroke();
-  }
   function onResize() {
     utils.onCanvasResize(canvas, (context: CanvasRenderingContext2D) => {
-      grid(context, 40);
-      hinomaru(context, 250, top + 100, 120);
+      utils.grid(canvas, top, context, 40);
+      utils.hinomaru(context, 250, top + 100, 120);
     });
   }
 
--- src/typescript/canvas2Dutils.ts
+++ src/typescript/canvas2Dutils.ts
@@ -3,7 +3,7 @@
 // from this file in future.
 /* eslint-disable import/prefer-default-export */
 
-export { onCanvasResize };
+export { onCanvasResize, hinomaru, grid };
 
 type CallbackToContext2D = (context2d: CanvasRenderingContext2D) => void;
 
@@ -17,4 +17,38 @@ function onCanvasResize(canvas: HTMLCanvasElement, drawToContext: CallbackToCont
 
     drawToContext(context);
   }
+}
+
+function hinomaru(context: CanvasRenderingContext2D, x: number, y: number, radius: number) {
+  context.fillStyle = '#ff0000';
+  context.beginPath();
+  context.arc(x, y, radius, 0, Math.PI * 2.0);
+  context.fill();
+}
+
+function grid(
+  canvas: HTMLCanvasElement,
+  top: number,
+  context: CanvasRenderingContext2D,
+  spacing: number,
+) {
+  const x0 = 0.5;
+  const x1 = canvas.width;
+  const y0 = top + 0.5;
+  const y1 = canvas.height;
+
+  context.beginPath();
+
+  for (let x = x0; x < x1; x += spacing) {
+    context.moveTo(x, y0);
+    context.lineTo(x, y1);
+  }
+
+  for (let y = y0; y < y1; y += spacing) {
+    context.moveTo(x0, y);
+    context.lineTo(x1, y);
+  }
+
+  context.strokeStyle = 'blue'; // "rgb(0.8,0.8,0.8)"; //#888; //"black";
+  context.stroke();
 }
