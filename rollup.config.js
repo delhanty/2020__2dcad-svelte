04cd0d7f0e9a088b9dabc0cf6c1d0d97ddd38d23
Paul Delhanty <paul.delhanty_git@cadcambridge.net>
2021-08-07 09:07:10 +0900

    Replace the abandoned `rollup-plugin-typescript2` by the very well-maintained `rollup-plugin-ts`.

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from 'rollup-plugin-ts';
 import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
